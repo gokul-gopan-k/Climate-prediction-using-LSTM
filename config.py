8d7908666959bebe4c7db58b26cf4e88ba159700
gokul-gopan-k <gokul.gopan.k@gmail.com>
2022-05-27 17:49:09 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+class CONFIG:
+    window_size = 60
+    BATCH_SIZE= 32
+    shuffle_buffer_size = 100
+    EPOCHS =150
+    data_path_csv = '../input/daily-climate-time-series-data/DailyDelhiClimateTrain.csv' 
+    train_test_split = 0.9
